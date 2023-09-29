298107f9b6a1b474bbbfa2587747e7fffff84051
raghunath <raghugreddy008@gmail.com>
2023-09-30 01:00:14 +0530

    update complete1

--- vars/dockerBuildComplete.groovy
+++ vars/dockerBuildComplete.groovy
@@ -2,6 +2,6 @@ def call(String git_branch = 'main', String git_credentials = null, String git_u
 {
     checkout([$class: 'GitSCM',
                 branches: [[name: "*/${git_branch}"]], 
-                userRemoteConfigs: [[credentialsId: "${git_credentials}", url: "${git_credentials}"]]
+                userRemoteConfigs: [[credentialsId: "${git_credentials}", url: "${git_url}"]]
                 ])
 }
