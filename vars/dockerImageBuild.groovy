e8d5bc72fa68e5920e60cf8c826548c13e651a0e
raghunath <raghugreddy008@gmail.com>
2023-10-02 20:46:55 +0530

    update7

--- vars/dockerImageBuild.groovy
+++ vars/dockerImageBuild.groovy
@@ -12,9 +12,9 @@ def call(String dockerfile = "Dockerfile",
 
                sh ''' 
                       docker images
+                      docker rmi ${image_name}:${image_tag}
                       docker image prune -f
                       docker builder prune -f
-                      docker rmi ${image_name}:${image_tag}
                       docker images 
                '''
 
